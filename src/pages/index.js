d28bd714c33edf18192925c979caf5d8e716278a
LeoYao <leoyao98@gmail.com>
2021-07-08 21:50:08 +1200

    add more post.mdx

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,42 @@ import Hero from '../components/Hero'
 import Layout from '../components/Layout'
 import Posts from '../components/Posts'
 import { graphql } from 'gatsby'
-const IndexPage = () => {
+
+export const query = graphql`
+  {
+    allMdx(limit: 3, sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        id
+        frontmatter {
+          title
+          category
+          date(formatString: "Do ,MMMM YYYY")
+          slug
+          readTime
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+          author
+        }
+        excerpt
+      }
+    }
+  }
+`
+
+const IndexPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
   return (
     <Layout>
       <Hero showPerson />
-
+      <Posts posts={posts} title="recently published" />
     </Layout>
   )
 }
+
 
 export default IndexPage
--- src/pages/posts.js
+++ src/pages/posts.js
@@ -3,8 +3,41 @@ import Layout from '../components/Layout'
 import Hero from '../components/Hero'
 import { graphql } from 'gatsby'
 import Posts from '../components/Posts'
-const PostsPage = () => {
-return <h4>posts page</h4>
+
+export const query = graphql`
+  {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+      nodes {
+        excerpt
+        frontmatter {
+          title
+          author
+          category
+          readTime
+          date(formatString: "MMMM, Do YYYY")
+          slug
+          image {
+            childImageSharp {
+              gatsbyImageData
+            }
+          }
+        }
+        id
+      }
+    }
+  }
+`
+
+const PostsPage = ({ data }) => {
+  const {
+    allMdx: { nodes: posts },
+  } = data
+  return (
+    <Layout>
+      <Hero />
+      <Posts posts={posts} title="all posts" />
+    </Layout>
+  )
 }
 
 
